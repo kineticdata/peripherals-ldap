5ded851be5a524dfcd4e0820c3bfb7990299b618
Nick <ndrafahl7@gmail.com>
2023-06-23 10:48:18 -0500

    KP-6837: Add additional tests for complex queries.

--- bridge-adapters/kinetic-bridgehub-adapter-ldap/src/test/java/com/kineticdata/bridgehub/adapter/ldap/LdapAdapterHelperTest.java
+++ bridge-adapters/kinetic-bridgehub-adapter-ldap/src/test/java/com/kineticdata/bridgehub/adapter/ldap/LdapAdapterHelperTest.java
@@ -218,6 +218,100 @@ public class LdapAdapterHelperTest {
     }
 
     @Test
+    public void test_buildFilter_ComplexFilter_withParams() {
+        LdapAdapter adapter = new LdapAdapter();
+
+        String query = "(&" +
+            "(|(employeeNumber=<%=parameter[\"Military ID\"]%>))" +
+            "(|(givenName=<%=parameter[\"First Name\"]%>))" +
+            "(|(sn=<%=parameter[\"Last Name\"]%>))" +
+            "(|(departmentNumber=<%=parameter[\"Installation\"]%>))" +
+            "(|(title=<%=parameter[\"Rank\"]%>))" +
+            "(|(employeeType=<%=parameter[\"VIP\"]%>))" +
+            "(|(mail=<%=parameter[\"Email\"]%>))" +
+        ")";
+
+        Map parameters = new HashMap();
+        parameters.put("Military ID", "12345");
+        parameters.put("First Name", "John");
+        parameters.put("Last Name", "Smith");
+        parameters.put("Installation", "DoD");
+        parameters.put("Rank", "Major");
+        parameters.put("VIP", "EPL");
+        parameters.put("Email", "*)(!(mail=*))");
+        String structure = "user";
+
+        String filter = null;
+        try {
+            filter = adapter.buildFilter(query, parameters, structure);
+        } catch (BridgeError e) {
+            BridgeError unexpectedError = e;
+        }
+
+        assertEquals(
+            String.format(
+                "(&(objectClass=user)(&(|(employeeNumber=%s))(|(givenName=%s))(|(sn=%s))(|(departmentNumber=%s))(|(title=%s))(|(employeeType=%s))(|(mail=%s))))",
+                parameters.get("Military ID"),
+                parameters.get("First Name"),
+                parameters.get("Last Name"),
+                parameters.get("Installation"),
+                parameters.get("Rank"),
+                parameters.get("VIP"),
+                parameters.get("Email")
+            ),
+            filter
+        );
+    }
+
+    @Test
+    public void test_buildFilter_ComplexFilter_withParams_2() {
+        LdapAdapter adapter = new LdapAdapter();
+
+        String query = "(&" +
+            "(sn=<%=parameter[\"Last Name\"]%>)" +
+            "(givenname=<%=parameter[\"First Name\"]%>)" +
+            "(|(SomeAttribute=ABC)(SomeAttribute=DEFG))" +
+            "(|" +
+            "(&" +
+            "(|(company=*)(department=<%=parameter[\"Company\"]%>))" +
+            "(|(mail=*domain.com)(mail=*DOMAIN.COM)(PersonGUID=*))" +
+            ")" +
+            "(&" +
+            "(|(mail=*.com)(mail=*.gov)(mail=*.COM)(mail=*.GOV))" +
+            "(!(mail=*domain.com))" +
+            "(!(mail=*DOMAIN.COM))" +
+            ")" +
+            "(|" +
+            "(&(mail=*)(!(mail=*.gov))(!(mail=*.GOV))(!(mail=*.com))(!(mail=*.COM)))" +
+            "(&(!(mail=*))(!(PersonGUID=*))" +
+            "(!(employeeID=*))(!(SomeAttribute2=*))" +
+        "))))";
+
+        Map parameters = new HashMap();
+        parameters.put("First Name", "John");
+        parameters.put("Last Name", "Smith");
+        parameters.put("Company", "MyCompany");
+        String structure = "user";
+
+        String filter = null;
+        try {
+            filter = adapter.buildFilter(query, parameters, structure);
+        } catch (BridgeError e) {
+            BridgeError unexpectedError = e;
+        }
+
+        assertEquals(
+            String.format(
+                "(&(objectClass=user)(&(sn=%s)(givenname=%s)(|(SomeAttribute=ABC)(SomeAttribute=DEFG))(|(&(|(company=*)(department=%s))(|(mail=*domain.com)(mail=*DOMAIN.COM)(PersonGUID=*)))(&(|(mail=*.com)(mail=*.gov)(mail=*.COM)(mail=*.GOV))(!(mail=*domain.com))(!(mail=*DOMAIN.COM)))(|(&(mail=*)(!(mail=*.gov))(!(mail=*.GOV))(!(mail=*.com))(!(mail=*.COM)))(&(!(mail=*))(!(PersonGUID=*))(!(employeeID=*))(!(SomeAttribute2=*)))))))",
+                parameters.get("Last Name"),
+                parameters.get("First Name"),
+                parameters.get("Company")
+            ),
+            filter
+        );
+    }
+
+    @Test
     public void test_buildSearchBase_emptyQuery() {
         LdapAdapter adapter = new LdapAdapter();
 
@@ -314,6 +408,74 @@ public class LdapAdapterHelperTest {
         String query = "<%=parameter[\"Added Search Base\"]%>(samaccountname=userid)";
         Map parameters = new HashMap();
         parameters.put("Added Search Base", "OU=Users");
+        String bridgeSearchBase = "OU=corp,DC=acme,DC=com";
+
+        String finalSearchBase = null;
+        try {
+            finalSearchBase = adapter.buildSearchBase(query, parameters, bridgeSearchBase);
+        } catch (BridgeError e) {
+            BridgeError unexpectedError = e;
+        }
+
+        assertEquals("OU=Users,OU=corp,DC=acme,DC=com", finalSearchBase);
+    }
+
+    @Test
+    public void test_buildSearchBase_ComplexFilter_withParams_NoAdditions() {
+        LdapAdapter adapter = new LdapAdapter();
+
+        String query = "(&" +
+            "(|(employeeNumber=<%=parameter[\"Military ID\"]%>))" +
+            "(|(givenName=<%=parameter[\"First Name\"]%>))" +
+            "(|(sn=<%=parameter[\"Last Name\"]%>))" +
+            "(|(departmentNumber=<%=parameter[\"Installation\"]%>))" +
+            "(|(title=<%=parameter[\"Rank\"]%>))" +
+            "(|(employeeType=<%=parameter[\"VIP\"]%>))" +
+            "(|(mail=<%=parameter[\"Email\"]%>))" +
+            ")";
+
+        Map parameters = new HashMap();
+        parameters.put("Military ID", "12345");
+        parameters.put("First Name", "John");
+        parameters.put("Last Name", "Smith");
+        parameters.put("Installation", "DoD");
+        parameters.put("Rank", "Major");
+        parameters.put("VIP", "EPL");
+        parameters.put("Email", "*)(!(mail=*))");
+        String bridgeSearchBase = "OU=corp,DC=acme,DC=com";
+
+        String finalSearchBase = null;
+        try {
+            finalSearchBase = adapter.buildSearchBase(query, parameters, bridgeSearchBase);
+        } catch (BridgeError e) {
+            BridgeError unexpectedError = e;
+        }
+
+        assertEquals("OU=corp,DC=acme,DC=com", finalSearchBase);
+    }
+
+    @Test
+    public void test_buildSearchBase_ComplexFilter_withParams_Additions() {
+        LdapAdapter adapter = new LdapAdapter();
+
+        String query = "OU=Users(&" +
+            "(|(employeeNumber=<%=parameter[\"Military ID\"]%>))" +
+            "(|(givenName=<%=parameter[\"First Name\"]%>))" +
+            "(|(sn=<%=parameter[\"Last Name\"]%>))" +
+            "(|(departmentNumber=<%=parameter[\"Installation\"]%>))" +
+            "(|(title=<%=parameter[\"Rank\"]%>))" +
+            "(|(employeeType=<%=parameter[\"VIP\"]%>))" +
+            "(|(mail=<%=parameter[\"Email\"]%>))" +
+            ")";
+
+        Map parameters = new HashMap();
+        parameters.put("Military ID", "12345");
+        parameters.put("First Name", "John");
+        parameters.put("Last Name", "Smith");
+        parameters.put("Installation", "DoD");
+        parameters.put("Rank", "Major");
+        parameters.put("VIP", "EPL");
+        parameters.put("Email", "*)(!(mail=*))");
         String bridgeSearchBase = "OU=corp,DC=acme,DC=com";
 
         String finalSearchBase = null;
